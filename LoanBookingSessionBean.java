22d171a395e74977a43cb3af04f97d40c7e98992
root <nchand8@gmail.com>
2022-02-06 06:30:32 +0000

    Third comit

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -3,7 +3,6 @@ import java.lang.Math;
 import java.io.printer; 
 import java.io.collection; 
 import java.io.scanner;
-import java.io.outputprinter;
 public class SpiralPatternExample2
 {  
 //function to print the spiral pattern  
